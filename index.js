a138018eb7723e9fcc50bf17e13d34deee66706c
Boss <s.sittilerdpaisal@gmail.com>
2017-05-26 17:05:38 +0700

    Change port

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require('express');
-const line = require('@line/bot-sdk');
+const app = express();
 
+const line = require('@line/bot-sdk');
+const middleware = require('@line/bot-sdk').middleware
 const config = {
   channelAccessToken: 'G6rZSbSUFOTJ+lk+azpu0BEOkyHAeiQzh7wimY6A3hnMuHQLI5Nh27KbddXHeUGguzHCrgX/Ne2wNNh/nEtQxWvO+agZ4NtVoCbEpeSNyd1AMtiKLULvHQwriP2ESjQVwk5ixBJt5zPzfYpO8Xa5IAdB04t89/1O/w1cDnyilFU=',
   channelSecret: 'U85f96c722276f3628ea09598da6153f7'
 };
 
-const app = express();
-app.post('/webhook', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result));
-});
+app.use(middleware(config));
+
+app.post('/webhook', line.middleware(config), (req, res) => res.json(req.body.events) );
 
 app.get('/',(req, res) => res.end('Site Working'));
 
 const client = new line.Client(config);
 function handleEvent(event) {
+
   if (event.type !== 'message' || event.message.type !== 'text') {
     return Promise.resolve(null);
   }
