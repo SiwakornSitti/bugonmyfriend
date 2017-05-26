24920802ad0994d252f613c4251625bb84a9a883
Boss <s.sittilerdpaisal@gmail.com>
2017-05-26 15:05:40 +0700

    Fixed default path

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.post('/webhook', line.middleware(config), (req, res) => {
     .then((result) => res.json(result));
 });
 
+app.get('/',(req, res) => res.end('Site Working'));
+
 const client = new line.Client(config);
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
